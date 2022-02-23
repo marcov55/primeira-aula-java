ec4d47230bef837c256432dc61c00679fc80b8d1
marcov55 <marcovascogit@gmail.com>
2022-02-23 17:56:11 +0000

    tipos primitivos

---
+++ Ola.java
@@ -0,0 +1,22 @@
+public class Ola {
+    private static int soma(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!!!");
+        System.out.println(soma(3, 5));
+    
+        boolean bol = false;
+        char ch = 'a';
+        byte intB = 2;
+        short intS = 200;
+        int intN = 1000;
+        long intL = 2000;
+        float fNum = 2.5Sf;
+        double dNum =2.5;
+
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MIN_VALUE);
+        
+     }
+}
